388a683c2b7d04fdb76a989d8f3eb0ea15fdd4a0
prabath1998 <prabhathudayanga1998@gmail.com>
2022-07-04 16:17:32 +0530

    changes in file service

--- src/app/services/file.service.ts
+++ src/app/services/file.service.ts
@@ -1,9 +1,33 @@
+import { HttpClient, HttpEvent } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FileService {
 
-  constructor() { }
+  private server = 'http://localhost:8080';
+
+  constructor(private http:HttpClient) { }
+
+  //upload files
+  upload(formData:FormData):Observable<HttpEvent<string[]>>{
+    return this.http.post<string[]>(`${this.server}/file/upload`,formData,{
+      reportProgress : true,
+      observe : 'events'
+    })
+
+  }
+
+
+  //download files
+  download(fileName:string):Observable<HttpEvent<Blob>>{
+    return this.http.get(`${this.server}/file/download/${fileName}`,{
+      reportProgress : true,
+      observe : 'events',
+      responseType : 'blob'
+    })
+
+  }
 }
